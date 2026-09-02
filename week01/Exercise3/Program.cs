3a81e58529b6c30fa7b87f76f80e314a7b353323
Victor <davik4life@gmail.com>
2026-09-02 22:51:36 +0100

    Exercise 3 Completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        /* 
+            What is the magic number?
+            What is your guess?
+            
+            Options:
+
+            Higher
+            Lower
+            You guessed it!
+        */
+        // Console.Write("What is your magic number? ");
+        // string magicNum = Console.ReadLine();
+
+        // Replaced User Magic Number Entry with generated numbers between 1 and 100.
+        int magicNumber = Random.Shared.Next(1, 101);
+
+        Console.Write("What is your guess? ");
+        string guessNum = Console.ReadLine();
+        int guess = int.Parse(guessNum);
+
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess? ");
+            guessNum = Console.ReadLine();
+            guess = int.Parse(guessNum);
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
