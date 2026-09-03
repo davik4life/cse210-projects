8874da48c50e55beefc61611359fab2fd9469bfd
Victor <davik4life@gmail.com>
2026-09-03 05:48:36 +0100

    Exercise 4 Stretch Challenge

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -11,6 +11,13 @@ class Program
         // Repeatedly prompt the user to enter a number until they enter 0.
         // After the user enters 0, display the sum, average, and total number of entries (excluding the 0) to the console.
         // Use a do-while loop to implement this functionality.
+
+        /*
+            Stretch Challenge
+            Have the user enter both positive and negative numbers, then find the smallest positive number (the positive number that is closest to zero).
+
+            Sort the numbers in the list and display the new, sorted list. Hint: There are C# libraries that can help you here, try searching the internet for them.
+        */
         do
         {
             Console.WriteLine("Enter a list of numbers, type 0 when finished.");
@@ -26,6 +33,9 @@ class Program
                 Console.WriteLine($"The sum is: {numbers.Sum()}");
                 Console.WriteLine($"The average is: {(float)numbers.Sum() / numbers.Count()}");
                 Console.WriteLine($"The largest number is: {numbers.Max()}");
+                Console.WriteLine($"The smallest positive number is: {numbers.Where(n => n > 0).Min()}");
+                numbers.Sort();
+                Console.WriteLine($"The sorted list is: [{string.Join(", ", numbers)}]");
                 break;
             }
         } while (true);
